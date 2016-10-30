e4da4360701aa3d1f387d2ed4ad7d0506164c697
David <david@enigmabridge.com>
2016-10-30 21:34:50 +0000

    payments page

---
+++ includes/form_6.php
@@ -0,0 +1,23 @@
+<?php	
+	if(empty($_POST['name6']) && strlen($_POST['name6']) == 0 || empty($_POST['email6']) && strlen($_POST['email6']) == 0)
+	{
+		return false;
+	}
+	
+	$name6 = $_POST['name6'];
+	$email6 = $_POST['email6'];
+	$message6 = $_POST['message6'];
+	
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from EB-webSite.";
+	$email_body = "You have received a new message. \n\n".
+				  "Name6: $name6 \nEmail6: $email6 \nMessage6: $message6 \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: contact@yoursite.com\n";
+	$headers .= "Reply-To: $email6";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
