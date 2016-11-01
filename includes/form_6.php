be5bb0a122b1ed2aec24b7c8f625c6ddf2dfcf4c
dancvrcek <dan@enigmabridge.com>
2016-11-01 00:49:43 +0000

    minor changes / types / broken links

--- includes/form_6.php
+++ includes/form_6.php
@@ -8,14 +8,14 @@
 	$email6 = $_POST['email6'];
 	$message6 = $_POST['message6'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'support@enigmabridge.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from EB-webSite.";
 	$email_body = "You have received a new message. \n\n".
 				  "Name6: $name6 \nEmail6: $email6 \nMessage6: $message6 \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
+	$headers .= "From: rsa@enigmabridge.com\n";
 	$headers .= "Reply-To: $email6";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
