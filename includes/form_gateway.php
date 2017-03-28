cdbbbc48f93d4e4dabb7181fb8ca25464618b852
dancvrcek <dan@enigmabridge.com>
2017-03-28 15:01:00 +0100

    new pricing for private space

---
+++ includes/form_gateway.php
@@ -0,0 +1,24 @@
+<?php	
+	if(empty($_POST['space_admin_email_gw']) && strlen($_POST['space_admin_email_gw']) == 0)
+	{
+		return false;
+	}
+	
+	$space_admin_email_gw = $_POST['space_admin_email_gw'];
+	$gatewaylocation = $_POST['gatewaylocation'];
+	$gatewaymessage = $_POST['gatewaymessage'];
+	$gatewaysize = $_POST['gatewaysize'];
+	
+	$to = 'enigmabridgecomsupport@enigmabridge.freshdesk.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from enigmabridge.com.";
+	$email_body = "You have received a new message. \n\n".
+				  "Space_Admin_Email_Gw: $space_admin_email_gw \nGatewaylocation: $gatewaylocation \nGatewaymessage: $gatewaymessage \nGatewaysize: $gatewaysize \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: privatespace@enigmabridge.com\n";
+	$headers .= "Reply-To: $space_admin_email_gw";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
---
+++ includes/form_office.php
@@ -0,0 +1,24 @@
+<?php	
+	if(empty($_POST['space_admin_email_off']) && strlen($_POST['space_admin_email_off']) == 0 || empty($_POST['officemessage']) && strlen($_POST['officemessage']) == 0)
+	{
+		return false;
+	}
+	
+	$space_admin_email_off = $_POST['space_admin_email_off'];
+	$officelocation = $_POST['officelocation'];
+	$officemessage = $_POST['officemessage'];
+	$officesize = $_POST['officesize'];
+	
+	$to = 'enigmabridgecomsupport@enigmabridge.freshdesk.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Message from enigmabridge.com.";
+	$email_body = "You have received a new message. \n\n".
+				  "Space_Admin_Email_Off: $space_admin_email_off \nOfficelocation: $officelocation \nOfficemessage: $officemessage \nOfficesize: $officesize \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: privatespace@enigmabridge.com\n";
+	$headers .= "Reply-To: $space_admin_email_off";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
